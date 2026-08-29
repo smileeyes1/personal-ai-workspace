346a7c4a284b7975f7c21798c5351bec47c3300e
smileeyes1 <smile.eyes1@gmail.com>
2026-08-29 11:30:30 +0300

    feat(auth): configure institutional Microsoft 365 client

---
+++ m365-config.js
@@ -0,0 +1,5 @@
+// Public Microsoft Entra application identifier for HAKIM EDU Ω.
+// This is an Application (client) ID, not a client secret.
+window.HAKIM_M365_CONFIG = Object.freeze({
+  clientId: 'c22595eb-cb64-4897-92a1-d49dc179064e'
+});
